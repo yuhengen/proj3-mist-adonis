2d49c2c9ee29b4a4745f6360a8d257c484849ae9
yuhengen <yuhengen@gmail.com>
2021-01-24 20:15:39 +0000

    Added processAdd to add new game to database

--- app/Controllers/Http/GameController.js
+++ app/Controllers/Http/GameController.js
@@ -18,6 +18,27 @@ class GameController {
   add({ view }) {
     return view.render('games/create')
   }
+
+  processAdd({ request, response }) {
+    let formData = request.post();
+    let newGame = new Game();
+
+    newGame.title = formData.title
+    newGame.price = formData.price*100
+    newGame.release_date = formData.release_date
+    newGame.description = formData.description
+    newGame.publisher = formData.publisher
+    newGame.developer = formData.developer
+    newGame.image = formData.image
+    newGame.preview_1 = formData.preview_1
+    newGame.preview_2 = formData.preview_2
+    newGame.preview_3 = formData.preview_3
+    newGame.preview_4 = formData.preview_4
+    newGame.preview_5 = formData.preview_5
+
+    newGame.save();
+    response.route('all_games')
+  }
 }
 
 module.exports = GameController
