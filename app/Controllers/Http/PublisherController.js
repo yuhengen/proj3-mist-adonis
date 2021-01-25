890c4c04cedd531e1a6b2a250dfae2b668d25729
yuhengen <yuhengen@gmail.com>
2021-01-25 06:43:59 +0000

    Added publisher login portal

--- app/Controllers/Http/PublisherController.js
+++ app/Controllers/Http/PublisherController.js
@@ -1,6 +1,11 @@
 'use strict'
 
+const Publisher = use('App/Models/Publisher')
+
 class PublisherController {
+  index({view}){
+    return view.render('publishers/index')
+  }
 }
 
 module.exports = PublisherController
